4fb2a3dbc737180ed504e0aa64263d745aa39e9f
alamincse97 <alamin.cse97@gmail.com>
2024-12-08 13:02:49 +0600

    added to hard comparition

--- Sheet_1 Data type - Conditions/Z. Hard Compare.cpp
+++ Sheet_1 Data type - Conditions/Z. Hard Compare.cpp
@@ -1,1 +1,20 @@
+#include<bits/stdc++.h>
 
+using namespace std;
+
+int main()
+{
+    long long a, b, c, d;
+    scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+
+    double ab = b * log(a);
+    double cd = d * log(c);
+
+    if(ab > cd){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+    return 0;
+}
