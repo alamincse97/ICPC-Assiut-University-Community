7d7a2a835decfaa97f80ea5260adc86e91195007
alamincse97 <alamin.cse97@gmail.com>
2024-12-08 10:17:21 +0600

    added to problem u

--- Sheet_1 Data type - Conditions/U. Float or int.cpp
+++ Sheet_1 Data type - Conditions/U. Float or int.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main()
+{
+    double n;
+    scanf("%lf", &n);
+
+    int integer = (int)n;
+    double decimal = n - integer;
+
+    if(n == integer)
+        printf("int %d", (int)n);
+    else
+        printf("float %d %.3lf", integer, decimal);
+
+    return 0;
+}
